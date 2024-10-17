8aa964f27ccbf1e770c6b09075279f870ae06700
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-10-16 21:03:41 -0300

    feat: create app component

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
+import Appointment from "./src/components/appointments/appointments";
+import AbaCalendar from "./src/screens/abacalendar/abacalendar";
+import AbaHome from "./src/screens/abahome/abahome";
+import AbaProfile from "./src/screens/abaprofile/abaprofile";
 import Login from "./src/screens/login/login";
+import Main from "./src/screens/main/main";
+import Schedule from "./src/screens/schedule/schedule";
+import Services from "./src/screens/services/services";
 
 export default function App() {
   return (
     <>
-      <Login />
+      <Schedule />
     </>
   );
 }
