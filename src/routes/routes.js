d2422c0e6812ec8ef9d0a4ba1e35343f8c183acd
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-10-21 16:54:21 -0300

    feat: create routes

---
+++ src/routes/routes.js
@@ -0,0 +1,11 @@
+import RoutesPrivate from "./routesPrivate.js";
+import RoutesOpen from "./routesOpen.js";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/auth.js";
+
+function Routes() {
+  const { user } = useContext(AuthContext);
+  return user.id_user ? <RoutesPrivate /> : <RoutesOpen />;
+}
+
+export default Routes;
