03324ad422993cfb2dc719b0890605625d161a77
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-10-21 19:44:24 -0300

    feat: add user registration and navigation to login

--- src/screens/account/account.jsx
+++ src/screens/account/account.jsx
@@ -1,9 +1,39 @@
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import icon from "../../constants/icon.js";
 import { styles } from "./account.style.js";
 import Button from "../../components/button/button.jsx";
+import { useState } from "react";
+import api from "../../constants/api.js";
 
-function Account() {
+function Account(props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function ExecuteAccont() {
+    try {
+      const response = await api.post("/users/register", {
+        name,
+        email,
+        password,
+      });
+
+      if (response.data) {
+        console.log(response.data);
+      }
+    } catch (error) {
+      if (error.response?.data.error) Alert.alert(error.response.data.error);
+      else Alert.alert("Ocorreu um erro! Tente novamente mais tarde");
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.containerLogo}>
@@ -12,24 +42,33 @@ function Account() {
 
       <View>
         <View style={styles.containerInput}>
-          <TextInput placeholder="Nome" style={styles.input} />
+          <TextInput
+            placeholder="Nome"
+            style={styles.input}
+            onChangeText={(text) => setName(text)}
+          />
         </View>
         <View style={styles.containerInput}>
-          <TextInput placeholder="E-mail" style={styles.input} />
+          <TextInput
+            placeholder="E-mail"
+            style={styles.input}
+            onChangeText={(text) => setEmail(text)}
+          />
         </View>
         <View style={styles.containerInput}>
           <TextInput
             placeholder="Senha"
             style={styles.input}
             secureTextEntry={true}
+            onChangeText={(text) => setPassword(text)}
           />
         </View>
-        <Button text="Criar conta" />
+        <Button text="Criar conta" onPress={ExecuteAccont} />
       </View>
 
       <View style={styles.footer}>
         <Text>Já tenho conta. </Text>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => props.navigation.goBack()}>
           <Text style={styles.footerLink}>Fazer login.</Text>
         </TouchableOpacity>
       </View>
