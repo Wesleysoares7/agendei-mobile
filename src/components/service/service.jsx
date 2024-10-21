5fb3ee7da6f1e5f4610f6a636bde809b826384e1
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-10-21 16:52:49 -0300

    feat: add onPress handler to schedule button

--- src/components/service/service.jsx
+++ src/components/service/service.jsx
@@ -16,7 +16,10 @@ function Service(props) {
       </View>
 
       <View style={styles.containerButton}>
-        <Button text="Agendar" />
+        <Button
+          text="Agendar"
+          onPress={() => props.onPress(props.id_service)}
+        />
       </View>
     </View>
   );
