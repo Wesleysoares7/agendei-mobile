a37bf481c0e947eaea93e5811dcbc2b1f61eeb33
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-10-21 16:56:08 -0300

    feat: integrate API for loading and deleting appointments

--- src/screens/abacalendar/abacalendar.jsx
+++ src/screens/abacalendar/abacalendar.jsx
@@ -1,21 +1,54 @@
 import { FlatList, View } from "react-native";
 import { styles } from "../abacalendar/abacalendar.style.js";
-import { appointments } from "../../constants/data";
 import Appointment from "../../components/appointments/appointments.jsx";
+import api from "../../constants/api.js";
+import { useEffect, useState } from "react";
 
 function AbaCalendar() {
+  const [appointments, setAppointments] = useState([]);
+
+  async function LoadAppointments() {
+    try {
+      const response = await api.get("/appointments");
+
+      if (response.data) setAppointments(response.data);
+    } catch (error) {
+      if (error.response?.data.error) Alert.alert(error.response.data.error);
+      else Alert.alert("Ocorreu um erro! Tente novamente mais tarde");
+    }
+  }
+
+  async function DeleteAppointment(id_appointment) {
+    try {
+      const response = await api.delete("/appointments/" + id_appointment);
+
+      if (response.data?.id_appointment) LoadAppointments();
+    } catch (error) {
+      if (error.response?.data.error) Alert.alert(error.response.data.error);
+      else Alert.alert("Ocorreu um erro! Tente novamente mais tarde");
+    }
+  }
+
+  useEffect(() => {
+    LoadAppointments();
+  }, []);
+
   return (
     <View style={styles.container}>
       <FlatList
         data={appointments}
-        keyExtractor={(appoint) => appoint.id_appointments}
+        keyExtractor={(appoint) => appoint.id_appointment}
         showsVerticalScrollIndicator={false}
         renderItem={({ item }) => {
           return (
             <Appointment
-              service={item.service}
+              id_appointment={item.id_appointment}
               doctor={item.doctor}
+              service={item.service}
               specialty={item.specialty}
+              bookingDate={item.booking_date}
+              bookingHour={item.booking_hour}
+              onPress={DeleteAppointment}
             />
           );
         }}
