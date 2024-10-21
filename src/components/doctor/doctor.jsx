192d5c2f63cf39d1f3108d2c274151bc3310a3a1
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-10-21 16:51:52 -0300

    feat: add dynamic icon and onPress handler

--- src/components/doctor/doctor.jsx
+++ src/components/doctor/doctor.jsx
@@ -1,10 +1,19 @@
 import { Image, TouchableOpacity, Text, View } from "react-native";
 import { styles } from "./doctor.style";
+import icon from "../../constants/icon.js";
 
 function Doctor(props) {
   return (
-    <TouchableOpacity style={styles.doctor}>
-      <Image source={props.icon} style={styles.icon} />
+    <TouchableOpacity
+      style={styles.doctor}
+      onPress={() =>
+        props.onPress(props.id_doctor, props.name, props.specialty, props.icon)
+      }
+    >
+      <Image
+        source={props.icon == "M" ? icon.male : icon.female}
+        style={styles.icon}
+      />
       <View>
         <Text style={styles.name}>{props.name}</Text>
         <Text style={styles.specialty}>{props.specialty}</Text>
