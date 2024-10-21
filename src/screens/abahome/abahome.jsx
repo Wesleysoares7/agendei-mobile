ae26aceb1eb633be2c790d03f381566a4f93d3ff
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-10-21 19:41:46 -0300

    feat: integrate API for loading doctors and navigate to services

--- src/screens/abahome/abahome.jsx
+++ src/screens/abahome/abahome.jsx
@@ -1,10 +1,38 @@
-import { FlatList, Text, View } from "react-native";
+import { Alert, FlatList, Text, View } from "react-native";
 import { styles } from "../abahome/abahome.style";
-import { doctors } from "../../constants/data";
 import Doctor from "../../components/doctor/doctor.jsx";
-import icon from "../../constants/icon.js";
+import { useEffect, useState } from "react";
+import api from "../../constants/api.js";
 
-function AbaHome() {
+function AbaHome(props) {
+  const [doctors, setDoctors] = useState([]);
+
+  function ClickDoctor(id_doctor, name, specialty, icon) {
+    props.navigation.navigate("services", {
+      id_doctor,
+      name,
+      specialty,
+      icon,
+    });
+  }
+
+  async function LoadDoctors() {
+    try {
+      const response = await api.get("/doctors");
+
+      if (response.data) {
+        setDoctors(response.data);
+      }
+    } catch (error) {
+      if (error.response?.data.error) Alert.alert(error.response.data.error);
+      else Alert.alert("Ocorreu um erro! Tente novamente mais tarde");
+    }
+  }
+
+  useEffect(() => {
+    LoadDoctors();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Agende seus serviços Médicos</Text>
@@ -16,9 +44,11 @@ function AbaHome() {
         renderItem={({ item }) => {
           return (
             <Doctor
+              id_doctor={item.id_doctor}
               name={item.name}
-              icon={item.icon == "M" ? icon.male : icon.female}
+              icon={item.icon}
               specialty={item.specialty}
+              onPress={ClickDoctor}
             />
           );
         }}
